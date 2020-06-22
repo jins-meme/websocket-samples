4ece90f25d0c22a7bdbd1fb038dd309d90e3d66e
jins-tkomoda <komoda@jins.com>
2020-06-22 10:16:53 +0900

    Added client version

---
+++ cli_send.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://127.0.0.1:5001');
+
+let counter = 0;
+ws.on('open', function open() {
+  setInterval(() => {
+    const bool_r1 = Math.random() > 0.985;
+    const rand_r1 = Math.floor(Math.random() * 30);
+    const rand_r2 = Math.random() * 7;
+    const data = {
+        blinkSpeed: bool_r1 ? 110 + Math.floor(Math.random() * 5) * 10 : 0,
+        blinkStrength: bool_r1 ? 30 + Math.floor(Math.random() * 60) : 0,
+        roll: 1.12 + rand_r1, pitch: 3.14 + rand_r1, yaw: 23.64 + rand_r1,
+        accX: -0.125, accY: rand_r2, accZ: -16.5, fitError: 0, walking: bool_r1 ? 1 : 0, noiseStatus: 0, powerLeft: 3,
+        eyeMoveUp: 0, eyeMoveDown: 0, eyeMoveLeft: 0, eyeMoveRight: 0, sequenceNumber: ++counter % 256
+    };
+    ws.send(JSON.stringify(data));   
+  },50)
+});
+
+ws.on('message', function incoming(data) {
+  console.log(`Msg from svr: ${data}`);
+});
--- svr_rec.js
+++ svr_rec.js
@@ -1,9 +1,14 @@
 const server = require("ws").Server;
 const ws_server = new server({ port: 5001 });
 
+let tsm1 = 0;
+
 ws_server.on("connection", ws => {
   console.log("connected from client");
   ws.on('message',function(message){
-      console.log("Received: " + message);
+    const ts = Date.now();
+    //console.log("Received:"  + (ts - tsm1) + "/" + message);
+    console.log(`Itvl:${ts - tsm1} Msg:${message}`);
+    tsm1 = ts;
   });
 });
